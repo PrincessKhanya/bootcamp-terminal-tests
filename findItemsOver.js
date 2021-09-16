12d9e295de98aa2585be7d3c040319ada143042e
Nokukhanya Makhanya <nokhanyamakhanya@gmail.com>
2021-09-16 10:05:53 +0200

    Fixed bug

--- findItemsOver.js
+++ findItemsOver.js
@@ -2,7 +2,6 @@ module.exports=module.exports=function findItemsOver20(itemList,n){
     var filteredObject=[];
     for (var i in itemList){
       var listItem=itemList[i];
-      console.log(listItem)
       if(listItem.qty>n){
         filteredObject.push(listItem)
       }
